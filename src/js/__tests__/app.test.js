ff9b8c237c05edd32345563ebb2b61fba7a755d9
DmitriyBaev <100maverick100@gmail.com>
2021-08-12 13:56:11 +0300

    init

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,10 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+const hitman = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+
+test('load', (done) => {
+  GameSavingLoader.load().then((data) => {
+    expect(data).toEqual(hitman);
+    done();
+  });
+});
