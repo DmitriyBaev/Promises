bdfa4e3c756823315d23c0191c96e2924e044774
DmitriyBaev <100maverick100@gmail.com>
2021-08-13 19:48:22 +0300

    fixed

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,8 +1,9 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then((data) => json(data));
+    return read().then((data) => json(data)).then((str) => new GameSaving(JSON.parse(str)));
   }
 }
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -4,7 +4,7 @@ const hitman = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman",
 
 test('load', (done) => {
   GameSavingLoader.load().then((data) => {
-    expect(data).toEqual(hitman);
+    expect(data).toEqual(JSON.parse(hitman));
     done();
   });
 });
